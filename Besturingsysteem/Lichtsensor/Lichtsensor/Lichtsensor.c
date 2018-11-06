fc5a04a09ec6c9de2333932c2757341e0f1c7d58
Marcelzinga <marcelzinga@live.nl>
2018-11-06 17:05:36 +0100

    Testen met interrupts

--- Besturingsysteem/Lichtsensor/Lichtsensor/Lichtsensor.c
+++ Besturingsysteem/Lichtsensor/Lichtsensor/Lichtsensor.c
@@ -15,25 +15,46 @@
 //prototypes
 uint8_t get_adc_value();
 void init_adc();
+void int_timer();
+// elke seconde gebeurd de functie
+
+volatile uint8_t i = 0;
+
+ISR (TIMER1_OVF_vect){
+
+	//printf("%i intensiteit=%d\n", i, getLight());
+	//i++;
+	//TCNT1 = 468750;
+}
 
 int main()
 {
+	init_timer();
 	ser_init();
 	ADC_init();
+	
+	
 	//DDRC = 0xDF; // PIN5 as Input
 	//DDRB = 0xFF;
 	
-	DDRC &= ~(1<<5); // set only pin 0 of port C as input
-	DDRB = _BV(DDB4); // set pin 4 of PORTB for Output
+	//DDRC &= ~(1<<5); // set only pin 0 of port C as input
+	//DDRB = _BV(DDB4); // set pin 4 of PORTB for Output
+
 	
-	
+	int i = 0;
 	while(1)
 	{
-		printf("intensiteit=%i \n", get_adc_value());
-		_delay_ms(200);
+		printf("%i intensiteit=%d \n", i, getLight());
+		i++;
+		_delay_ms(50000);
 	}
 	
 	
+}
+
+void getLight(){
+	uint8_t temp = get_adc_value();
+	return temp;
 }
 
 
@@ -53,4 +74,15 @@ uint8_t get_adc_value()
 	ADCSRA |= (1<<ADSC); // start conversion
 	loop_until_bit_is_clear(ADCSRA, ADSC);
 	return ADCH; // 8bit resolution, left adjusted
+}
+
+void init_timer (void){
+	//prescale op 1024
+	TCCR1B = 0x05;
+	sei();
+
+	TCNT1 = 468750; 
+	TIMSK1 |= (1 << TOIE1);
+	//TIMSK1 = 1 << OCIE1A; // Timer 1 Output Compare A Match Interrupt Enable
+	//OCR1A = (uint16_t)62500; // 1 sec = (256/16.000.000)*62499
 }
